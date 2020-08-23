6a992ef7774d77e3538ac2eaf0b47475f1b40e2a
Daniel Lwetabe <acehanks@gmail.com>
2020-08-23 20:03:31 +0300

    add repository interface to interact with database

--- account/service.go
+++ account/service.go
@@ -2,6 +2,7 @@ package account
 
 import "context"
 
+// Service is exposed to the external traffic
 type Service interface {
 	CreateUser(ctx context.Context, email, password string) (string, error)
 	GetUser(ctx context.Context, id string) (string, error)
--- account/user.go
+++ account/user.go
@@ -1,7 +1,16 @@
 package account
 
+import "context"
+
+// User model
 type User struct {
 	ID       string `json:"id,omitempty"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+// Repository connects to database
+type Repository interface {
+	CreateUser(ctx context.Context, user User) error
+	GetUser(ctx context.Context, id string) (string, error)
 }
